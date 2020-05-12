380d7664fa7cc6020bbb57e831606bc227168415
hsjhita1 <hjhita1997@gmail.com>
2020-05-12 11:01:21 +0100

    Upload of a working front and backend for adding posts to the database

--- Flask/application/__init__.py
+++ Flask/application/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=getenv('FLASK_TEST_DB_URI')
+app.config['SECRET_KEY'] = getenv('FLASK_SECRET_KEY')
 
 db = SQLAlchemy(app)
 
--- Flask/application/routes.py
+++ Flask/application/routes.py
@@ -1,12 +1,14 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from application import app
 from application.models import Posts
+from application.forms import PostForm
+from application import db
 
 @app.route('/')
 @app.route('/home')
 def home():
-    postData = Posts.query.first()
-    return render_template('home.html', title='Home Page', post=postData)
+    postData = Posts.query.all()
+    return render_template('home.html', title='Home Page', posts=postData)
 
 @app.route('/login')
 def login():
@@ -15,3 +17,21 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html', title='Register Page')
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        print(form.errors)
+    return render_template('post.html', title='Post', form = form)
